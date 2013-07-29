c32a6c0ed1e9e41f002cb404e985a8ddcbe132a3
Thomas Grenfell Smith <thomathom@gmail.com>
2013-07-29 13:06:54 -0400

    Many pages, each particular per user, int ids
    I guess I included a whole form validation library, which might have
    been excessive.

--- coursebuilder/modules/wikifolios/wiki_models.py
+++ coursebuilder/modules/wikifolios/wiki_models.py
@@ -2,6 +2,22 @@ from models.models import BaseEntity
 from google.appengine.ext import db
 
 class WikiPage(BaseEntity):
-    title = db.StringProperty()
     text = db.TextProperty()
+    unit = db.IntegerProperty()
+
+    @classmethod
+    def get_key(cls, user, unit=None):
+        if unit:
+            return db.Key.from_path(
+                    'Student', user.key().name(),
+                    'WikiPage', 'unit:%d' % unit)
+        else:
+            return db.Key.from_path(
+                    'Student', user.key().name(),
+                    'WikiPage', 'profile')
+
+    @classmethod
+    def get_page(cls, user, unit=None):
+        key = cls.get_key(user, unit)
+        return cls.get(key)
 
--- coursebuilder/modules/wikifolios/wikifolios.py
+++ coursebuilder/modules/wikifolios/wikifolios.py
@@ -7,46 +7,93 @@ import bleach
 import webapp2
 from controllers.utils import BaseHandler, ReflectiveRequestHandler
 from modules.wikifolios.wiki_models import WikiPage
+import logging
+import urllib
+import wtforms as wtf
 
-class TestHandler(BaseHandler, ReflectiveRequestHandler):
+class WikiNavForm(wtf.Form):
+    unit = wtf.IntegerField()
+
+def try_parse_int(s, default=None, base=10):
+    try:
+        return int(s, base)
+    except ValueError:
+        return default
+
+class WikiHandler(BaseHandler, ReflectiveRequestHandler):
     default_action = "view"
     get_actions = ["view", "edit"]
     post_actions = ["save"]
 
+    def _set_unit(self):
+        form = WikiNavForm(self.request.params)
+        if form.validate():
+            self.unit = form.unit.data
+        else:
+            self.unit = None
+
+    def _find_page(self, student, create=False):
+        key = WikiPage.get_key(student, self.unit)
+
+        page = WikiPage.get(key)
+        if (not page) and create:
+            page = WikiPage(key=key)
+
+        return page
+
+    def _create_action_url(self, action="view"):
+        unit = ''
+        if self.unit:
+            unit = self.unit
+        params = {
+                'action': action,
+                'unit': unit,
+                }
+        return '?'.join((
+                self.request.path,
+                urllib.urlencode(params)))
+
     def get_view(self):
-        user = self.personalize_page_and_get_enrolled()
+        student = self.personalize_page_and_get_enrolled()
+        self._set_unit()
         content = None
 
-        page = WikiPage.get_by_key_name("test")
+        page = self._find_page(student)
         if page:
             content = page.text
         if not content:
-            content = bleach.clean('an <script>evil()</script> example')
+            content = bleach.clean('(no page here yet)')
         self.template_value['content'] = content
         self.template_value['can_edit'] = True
+        self.template_value['edit_url'] = self._create_action_url('edit')
         self.template_value['navbar'] = {'wiki': True}
         self.render("wf_page.html")
 
     def get_edit(self):
-        user = self.personalize_page_and_get_enrolled()
-        page = WikiPage.get_by_key_name("test")
+        student = self.personalize_page_and_get_enrolled()
+        self._set_unit()
+
+        page = self._find_page(student)
         if page:
             content = page.text
         else:
             content = ''
         self.template_value['content'] = content
+        self.template_value['unit'] = self.unit
         self.template_value['navbar'] = {'wiki': True}
         self.template_value['xsrf_token'] = self.create_xsrf_token('save')
         self.render("wf_edit.html")
 
     def post_save(self):
-        user = self.personalize_page_and_get_enrolled()
-        page = WikiPage.get_by_key_name("test")
-        if not page:
-            page = WikiPage(key_name="test")
+        student = self.personalize_page_and_get_enrolled()
+        self._set_unit()
+        page = self._find_page(student, create=True)
+
         page.text = bleach.clean(self.request.get('text', ''))
+        page.unit = self.unit
+
         page.put()
-        self.redirect("wiki")
+        self.redirect(self._create_action_url('view'))
 
 
 
@@ -56,7 +103,7 @@ def register_module():
     global module
 
     handlers = [
-            ('/wiki', TestHandler),
+            ('/wiki', WikiHandler),
             ]
     # def __init__(self, name, desc, global_routes, namespaced_routes):
     module = custom_modules.Module("Wikifolios", "Wikifolio pages",
