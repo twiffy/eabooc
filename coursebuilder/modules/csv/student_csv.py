64e76fac7646daa76fd17719668b5e6f39df1342
Thomas Grenfell Smith <thomathom@gmail.com>
2013-08-02 14:40:50 -0400

    Download a csv list of all students

---
+++ coursebuilder/modules/csv/student_csv.py
@@ -0,0 +1,41 @@
+from models import custom_modules
+from models.models import Student
+from models.roles import Roles
+from controllers.utils import BaseHandler
+from google.appengine.ext import db
+import logging
+import csv
+
+
+class StudentCsvHandler(BaseHandler):
+    def get(self):
+        if not Roles.is_super_admin():
+            self.error(404)
+            return
+        props = sorted(Student.properties().keys() + ['email',])
+        self.response.content_type = 'text/plain'
+        out = csv.DictWriter(self.response, props, extrasaction='ignore')
+        out.writeheader()
+        for s in Student.all().run(limit=9999):
+            d = db.to_dict(s)
+            d['email'] = s.key().name()
+            for p in d.keys():
+                if type(d[p]) is list:
+                    d[p] = ", ".join(d[p])
+            out.writerow(d)
+
+
+module = None
+
+def register_module():
+    global module
+
+    handlers = [
+            ('/student_csv', StudentCsvHandler),
+            ]
+    # def __init__(self, name, desc, global_routes, namespaced_routes):
+    module = custom_modules.Module("Student CSV", "Student CSV",
+            [], handlers)
+
+    return module
+
