10d2ef5d372902816809bdc762844df3755ff829
Thomas Grenfell Smith <thomathom@gmail.com>
2013-08-15 15:21:10 -0400

    Lesson fetching script

---
+++ coursebuilder/fetch-lessons.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eux
+
+curl 'https://docs.google.com/document/d/108pEO5Cc4RcLgEGvJiOI0X9Htc5xksURKgpOGmGhuuw/pub?embedded=true' > assets/content/u1l3.html
+curl 'https://docs.google.com/document/d/1_Hil0YsHay1Jq6ts86f-l4ezvrm6BtzCbO_QfgL2hBs/pub?embedded=true' > assets/content/u2l1.html
+# 3
+# 4
+curl 'https://docs.google.com/document/d/15x85z-c1wxFWqmsTZs3dwiNBYB88h_l3oCs3Mat6qls/pub?embedded=true' > assets/content/u5l1.html
+# 6
+# 7
+curl 'https://docs.google.com/document/d/1OBsZAFxIm5S9dcuSV9qvb-4AP9lP1qKSHRbo5QgcPh8/pub?embedded=true' > assets/content/u8l1.html
+# 9
+# 10
+# 11
+
