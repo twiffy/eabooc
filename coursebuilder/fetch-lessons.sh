1d94bfb6fa79abdee0e5e454ca53df3730b7abaa
Thomas Grenfell Smith <thomathom@gmail.com>
2013-08-16 10:12:33 -0400

    Fix lessons names

--- coursebuilder/fetch-lessons.sh
+++ coursebuilder/fetch-lessons.sh
@@ -2,7 +2,8 @@
 set -eux
 
 curl 'https://docs.google.com/document/d/108pEO5Cc4RcLgEGvJiOI0X9Htc5xksURKgpOGmGhuuw/pub?embedded=true' > assets/content/u1l3.html
-curl 'https://docs.google.com/document/d/1_Hil0YsHay1Jq6ts86f-l4ezvrm6BtzCbO_QfgL2hBs/pub?embedded=true' > assets/content/u2l1.html
+curl 'https://docs.google.com/document/d/111XNs8Ftc2TnZ374W_UJg2MKfhKOSgOwpwe33NHjnow/pub?embedded=true' > assets/content/u2l1.html
+curl 'https://docs.google.com/document/d/1uiNj4C3iqxXC79H93R15u1TSeZ86mgabJJjAzB3wpOw/pub?embedded=true' > assets/content/u2l2.html
 # 3
 # 4
 curl 'https://docs.google.com/document/d/15x85z-c1wxFWqmsTZs3dwiNBYB88h_l3oCs3Mat6qls/pub?embedded=true' > assets/content/u5l1.html
