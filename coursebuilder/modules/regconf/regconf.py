fafe5c8fc40c786ba66b30b74bc81a15ad43d923
Thomas Grenfell Smith <thomathom@gmail.com>
2013-08-16 12:00:24 -0400

    Tweaks to registration wording and visuals

--- coursebuilder/modules/regconf/regconf.py
+++ coursebuilder/modules/regconf/regconf.py
@@ -119,6 +119,7 @@ class ConfirmationHandler(BaseHandler):
     def do_render(self, form, template):
         self.template_value['navbar'] = {'registration': True}
         self.template_value['form'] = form
+        self.template_value['list'] = list
         self.template_value['xsrf_token'] = (
             XsrfTokenManager.create_xsrf_token('register-conf-post-' + self._page()))
         self.render(template)
