9ba4cae0eeeae6d17df77c405753a9b125e0ea4d
Thomas Grenfell Smith <thomathom@gmail.com>
2013-09-05 15:29:11 -0400

    Add 'can use location' field to student csv

--- coursebuilder/modules/csv/student_csv.py
+++ coursebuilder/modules/csv/student_csv.py
@@ -1,10 +1,17 @@
 from models import custom_modules
 from models.models import Student
 from models.roles import Roles
+from modules.regconf.regconf import FormSubmission
 from controllers.utils import BaseHandler
 from google.appengine.ext import db
 import logging
 import csv
+
+def find_can_use_location(student):
+    conf_submission = FormSubmission.all().filter('user =', student.key()).filter('form_name =', 'conf').get()
+    if conf_submission:
+        return conf_submission.accept_location
+
 
 
 class StudentCsvHandler(BaseHandler):
@@ -16,12 +23,15 @@ class StudentCsvHandler(BaseHandler):
             self.response.write("NO")
             return
 
-        props = sorted(Student.properties().keys() + ['email',])
+        props = sorted(Student.properties().keys() + ['email', 'can_use_location'])
         out = csv.DictWriter(self.response, props, extrasaction='ignore')
         out.writeheader()
         for s in Student.all().run(limit=9999):
             d = db.to_dict(s)
             d['email'] = s.key().name()
+            if d.get('is_participant', False):
+                d['can_use_location'] = find_can_use_location(s)
+
             for p in d.keys():
                 if type(d[p]) is list:
                     d[p] = u", ".join(d[p])
