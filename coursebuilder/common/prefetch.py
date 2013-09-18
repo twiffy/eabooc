7201c0e3beed39be54c687388789b80aef92acf7
Thomas Grenfell Smith <thomathom@gmail.com>
2013-09-18 11:58:34 -0400

    well, it used to work, why did that change?

--- coursebuilder/common/prefetch.py
+++ coursebuilder/common/prefetch.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import db
+import logging
 def prefetch_refprops(entities, *props):
     fields = [(entity, prop) for entity in entities for prop in props]
     ref_keys = [prop.get_value_for_datastore(x) for x, prop in fields]
@@ -14,14 +15,18 @@ class CachingPrefetcher(object):
     def add(self, obj):
         self.cache[obj.key()] = obj
 
-    def prefetch(self, entities, *props):
+    def prefetch(self, entities_iterable, *props):
+        entities = list(entities_iterable)
         fields = [(entity, prop) for entity in entities for prop in props]
         ref_keys = set([prop.get_value_for_datastore(x) for x, prop in fields])
         keys_to_fetch = ref_keys - self.cache.viewkeys()
 
         if keys_to_fetch:
+            #logging.warning('fetching %d entities', len(keys_to_fetch))
             ref_entities = dict((x.key(), x) for x in db.get(keys_to_fetch))
             self.cache.update(ref_entities)
+        #else:
+            #logging.warning('Not fetching any entities, they all cached bro')
 
         for (entity, prop), ref_key in zip(fields, ref_keys):
             prop.__set__(entity, self.cache[ref_key])
