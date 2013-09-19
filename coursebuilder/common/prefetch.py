bebfb5b765a712fc3c870a283ef7c5f218796827
Thomas Grenfell Smith <thomathom@gmail.com>
2013-09-19 11:06:42 -0400

    better debugging for prefetcher

--- coursebuilder/common/prefetch.py
+++ coursebuilder/common/prefetch.py
@@ -17,18 +17,24 @@ class CachingPrefetcher(object):
 
     def prefetch(self, entities_iterable, *props):
         entities = list(entities_iterable)
+        if len(entities) == 0:
+            return entities
         fields = [(entity, prop) for entity in entities for prop in props]
+        #logging.warning('there are %d (%d) entities of type %s',
+                #len(entities),
+                #len(fields),
+                #type(entities[0]).__name__)
         ref_keys = [prop.get_value_for_datastore(x) for x, prop in fields]
         ref_key_set = set(ref_keys)
         keys_to_fetch = ref_key_set - self.cache.viewkeys()
 
         if keys_to_fetch:
-            #logging.warning('fetching %d entities', len(keys_to_fetch))
+            #logging.warning('fetching %d entities on type %s', len(keys_to_fetch), type(entities[0]).__name__)
             ref_entities = dict((x.key(), x) for x in db.get(keys_to_fetch))
             self.cache.update(ref_entities)
         #else:
-            #logging.warning('Not fetching any entities, they all cached bro')
-
+            #logging.warning('Not fetching any entities for %s, they all cached bro', type(entities[0]).__name__)
         for (entity, prop), ref_key in zip(fields, ref_keys):
             prop.__set__(entity, self.cache[ref_key])
+
         return entities
