4e1245816155109ff6caba52540ff707f4c9b3b7
Thomas Grenfell Smith <thomathom@gmail.com>
2013-10-16 15:15:25 -0400

    Per-unit evidence page in there

--- coursebuilder/modules/wikifolios/report_handlers.py
+++ coursebuilder/modules/wikifolios/report_handlers.py
@@ -49,6 +49,7 @@ class EvidenceHandler(BaseHandler):
         self.template_value['fields'] = {
                 k: Markup(v) for k,v in transforms.loads(self.unit.wiki_fields).iteritems()}
         self.template_value['unit'] = self.find_unit_by_id(self.unit_num)
+        self.template_value['report'] = self.unit
         self.template_value['layout_template'] = 'wf_evidence.html'
         self.render(page_templates.templates[self.unit_num])
 
