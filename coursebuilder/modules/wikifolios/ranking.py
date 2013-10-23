f94b2f8c9430a8448be51c5d51028f0669f504cd
Thomas Grenfell Smith <thomathom@gmail.com>
2013-10-23 11:03:28 -0400

    Initial version of integer-based ranking field

--- coursebuilder/modules/wikifolios/ranking.py
+++ coursebuilder/modules/wikifolios/ranking.py
@@ -1,5 +1,8 @@
 import wtforms as wtf
-from wtforms.ext.appengine.fields import StringListPropertyField
+from wtforms.ext.appengine.fields import StringListPropertyField, IntegerListPropertyField
+from jinja2 import Markup
+from wtforms.widgets.core import html_params, HTMLString, TextArea
+from unittest import TestCase
 
 class StringRankingField(StringListPropertyField):
     def __init__(self, label=None, validators=None, choices=None, **kwargs):
@@ -18,10 +21,69 @@ class StringRankingField(StringListPropertyField):
                     raise ValueError("'%(value)s' is duplicated, but you can only rank it once" % dict(value=d))
 
 
-if __name__ == '__main__':
+class IntegerRankingWidget(object):
+    def __call__(self, field, **kwargs):
+        class_ = kwargs.get('class', 'integer-ranking')
+
+        html = [Markup('<div class="%s">') % class_]
+                
+        html.append('<ol %s>' % html_params(**kwargs))
+        for choice in field.iter_choices():
+            html.append(Markup('<li>%s</li>') % choice)
+        html.append('</ol>')
+        html.append(TextArea()(field, **kwargs))
+        html.append('</div>')
+        return u''.join(
+                unicode(x) for x in html)
+
+
+class IntegerRankingField(IntegerListPropertyField):
+    widget = IntegerRankingWidget()
+    def __init__(self, label=None, validators=None, choices=None, **kwargs):
+        super(IntegerRankingField, self).__init__(label, validators, **kwargs)
+        self.choices = choices
+
+    def iter_choices(self):
+        return iter(self.choices)
+
+    def process_formdata(self, valuelist):
+        # this is broken : it should happen after super() and iterate on .data....
+        super(IntegerRankingField, self).process_formdata(valuelist)
+        if self.data:
+            index_list = self.data
+            self.data = []
+            try:
+                for i in index_list:
+                    if i < 1:
+                        raise ValueError()
+                    self.data.append(self.choices[i - 1])
+            except (ValueError, IndexError):
+                self.data = None
+                raise ValueError('Not a valid item number')
+
+
+# --------- tests ----------
+
+class DummyPostData(dict):
+    def getlist(self, key):
+        v = self[key]
+        if not isinstance(v, (list, tuple)):
+            v = [v]
+        return v
+
+class IntegerRankingFieldTest(TestCase):
     class F(wtf.Form):
-        srf = StringRankingField('Awesome', choices=[('a', 'The Letter A'), ('b', 'The Letter B')])
+        a = IntegerRankingField(choices=['cheese', 'ham'])
 
-    for fld in F():
-        print fld
+    def test_with_data(self):
+        form = self.F(DummyPostData(a=['2\n1']))
+        self.assertEqual(form.a.data, ['ham', 'cheese'])
+        self.assertEqual(form.a(), '''<div class="integer-ranking"><ol ><li>cheese</li><li>ham</li></ol><textarea id="a" name="a">2\n1</textarea></div>''')
 
+    def test_bad_data(self):
+        form = self.F(DummyPostData(a=['3']))
+        self.assertEqual(form.a.data, None)
+
+    def test_negative(self):
+        form = self.F(DummyPostData(a='-1'))
+        self.assertEqual(form.a.data, None)
