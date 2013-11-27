d1b5b35d56254c7c7aa9a8757f6e3b9e7105f3e7
Thomas Grenfell Smith <thomathom@gmail.com>
2013-11-27 13:14:46 -0500

    First page of end of course survey

--- coursebuilder/modules/regconf/exit_survey.py
+++ coursebuilder/modules/regconf/exit_survey.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from regconf import FormSubmission
 from controllers.utils import BaseHandler, XsrfTokenManager
 import webapp2
 from google.appengine.ext import db
 import wtforms as wtf
+
+reuse_previous_submissions = False
 
 class SurveyHandler(BaseHandler):
     form = None
@@ -17,10 +21,13 @@ class SurveyHandler(BaseHandler):
         if not user:
             return
 
-        sub_query = FormSubmission.all()
-        sub_query.filter('form_name', self.name)
-        sub_query.filter('user', user.key())
-        prev_submission = sub_query.order('-submitted').get()
+        if reuse_previous_submissions:
+            sub_query = FormSubmission.all()
+            sub_query.filter('form_name', self.name)
+            sub_query.filter('user', user.key())
+            prev_submission = sub_query.order('-submitted').get()
+        else:
+            prev_submission = None
 
         form = self.form(None, prev_submission)
 
@@ -47,13 +54,68 @@ class SurveyHandler(BaseHandler):
             for k,v in form.data.items():
                 setattr(submission, k, db.Text(v))
             submission.put()
-            self.action(self, user, form)
+            self.action(user, form)
         else:
             # Validation errors will be included in the 'form' object
-            self.do_render(form, self.templates[page])
+            self.do_render(form)
 
 class TryhardForm(wtf.Form):
-    a = wtf.IntegerField()
+    first_hear = wtf.RadioField(
+            "Where did you first hear about this course?",
+            choices=(
+                ('facebook', 'Facebook'),
+                ('twitter', 'Twitter'),
+                ('linkedin', 'LinkedIn'),
+                ('google-ads', 'Google Ads'),
+                ('hastac', 'HASTAC'),
+                ('word-of-mouth', 'Word of Mouth (please explain)'),
+                ('other', 'Other (please explain)'),
+                ),
+            validators=(wtf.validators.required(),))
+    first_hear_explain = wtf.StringField(
+            validators=[wtf.validators.optional()])
+
+    other_courses = wtf.RadioField(
+            "Before enrolling in this course, had you enrolled in another online course?",
+            choices=(
+                ('yes', 'Yes'),
+                ('no', 'No'),
+                ),
+            validators=[wtf.validators.required()])
+    other_courses_how_many = wtf.SelectField(
+            "If so, how many?",
+            choices=(
+                [(str(n), str(n)) for n in range(1,11)]
+                + [('11+', '11 or more')]),
+            validators=[wtf.validators.optional()])
+    other_courses_what = wtf.TextAreaField(
+            "If so, what courses and where?",
+            validators=[wtf.validators.optional()])
+
+    enroll_motivation = wtf.RadioField(
+            "What factor most motivated you to <i>enroll</i> in this course?",
+            choices=(
+                ('expert-badges', 'Earning assessment expert & expertise badges'),
+                ('leader-badges', 'Earning assessment leader badges'),
+                ('certificate', 'Earning the instructor verified certificate'),
+                ('required', 'I was required to by my job'),
+                ('peers', 'Interacting with peers about assessment'),
+                ('instructor', u'The instructor’s reputation '),
+                ('learning', 'Learning about assessment   '),
+                ('taking-mooc', 'Taking an open online course'),
+                ('other', 'Other (please specify)'),
+                ),
+            validators=[wtf.validators.required()])
+    enroll_motivation_other = wtf.StringField(
+            validators=[wtf.validators.optional()])
+
+    complete_motivation = wtf.RadioField(
+            "What factor most motivated you to <i>complete</i> this course?",
+            choices=enroll_motivation.kwargs['choices'],
+            validators=[wtf.validators.required()])
+    complete_motivation_other = wtf.StringField(
+            validators=[wtf.validators.optional()])
+
 
 class TryhardSurveyHandler(SurveyHandler):
     form = TryhardForm
